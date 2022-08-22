9f4164f63ffd24ae4fc9d0ce8fb3370e4118294c
Jorge Ronald Cribb <jorge.cribb@gmail.com>
2022-08-22 09:38:52 -0300

    Creación 06

---
+++ 06-deses-obj.js
@@ -0,0 +1,22 @@
+const persona = {
+    nombre: 'Tony',
+    edad: 45,
+    clave: 'Ironman',
+    latlng: {
+        lat: 14.28,
+        lng: 10.11,
+    }
+}
+
+const retornaPersona = ({clave, nombre, edad, rango='Capitán'})=> {
+    return {nombreClave: clave, anios: edad }
+}
+const useContext = ({clave, latlng, nombre, edad, rango='Capitán'})=> {
+    return {nombreClave: clave, anios: edad , latlng}
+}
+//const avenger = retornaPersona(persona);
+//console.log( avenger)
+// const {nombreClave,anios} = retornaPersona(persona);
+//console.log(nombreClave,anios);
+const {nombreClave,anios, latlng: {lat,lng}} = useContext(persona);
+console.log(nombreClave,anios,lat,lng);
